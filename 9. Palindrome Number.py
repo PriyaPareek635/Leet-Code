f7cfc084c3339686f012e274b84315f294f73fdb
unknown <priyapareek635@gmail.com>
2021-04-06 20:34:23 +0530

    updating removed files

--- 9. Palindrome Number.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        s = str(x)
-        rs = "".join(list(reversed(s)))
-        if rs != s:
-            return False
-        return True
-
-        
